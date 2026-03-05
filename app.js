518fdd3bc244a2da1e41ab59d463a6d570b55272
Joshua <246270231+schneebedeckte-eule@users.noreply.github.com>
2026-03-05 15:40:01 +0100

    add output

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ let height = parseInt(prompt("Enter your height in cm")) / 100;
 let gender = prompt("Enter your gender as M or F");
 let bmiCategory;
 
-// let firstName = prompt("Enter your first name");
-// let lastName = prompt("Enter your last name");
-// let middleName = null;
-
 //BMI CALCULATION
+
 let bmi = weight / height ** 2;
 
 //OUTPUT
-console.log(bmi);
+let result = `Your weight is ${weight}kg and your height is ${height * 100}cm or ${height}m. Therefore your BMI is ${bmi}.`;
+console.log(result);
