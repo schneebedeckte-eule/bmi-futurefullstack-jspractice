db5f61c370b6bdacd476934658632ff3e7fe1b92
Joshua <246270231+schneebedeckte-eule@users.noreply.github.com>
2026-03-05 14:58:26 +0100

    add calculations

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-let weight = parseInt(prompt("Enter your weight"));
-let height = parseInt(prompt("Enter your height"));
+// VARIABLES
+
+let weight = parseInt(prompt("Enter your weight in kg"));
+let height = parseInt(prompt("Enter your height in cm")) / 100;
 let gender = prompt("Enter your gender as M or F");
 let bmiCategory;
 
-let firstName = prompt("Enter your first name");
-let lastName = prompt("Enter your last name");
-let middleName = null;
+// let firstName = prompt("Enter your first name");
+// let lastName = prompt("Enter your last name");
+// let middleName = null;
+
+//BMI CALCULATION
+let bmi = weight / height ** 2;
+
+//OUTPUT
+console.log(bmi);
